8f39964a6eabb6b2e58853fc862ff26f2fc2741f
Tristan Camejo <twisttaan@icloud.com>
2021-11-17 17:45:20 +1100

    Base

--- next.config.js
+++ next.config.js
@@ -1,2 +1,2 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
+const withNextra = require('nextra')('nextra-theme-blog', './theme.config.tsx')
 module.exports = withNextra()
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'TristanSMP',
+    site_url: 'https://tristansmp.com',
+    feed_url: 'https://tristansmp.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
