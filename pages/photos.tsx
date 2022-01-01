aa1523de931b0a6bb9bfee0872ee5f51aac45448
Tristan Camejo <twisttaan@icloud.com>
2022-01-01 13:58:46 +1100

    feat: Show username on photos with tooltip for discord tag

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -1,3 +1,5 @@
+import { Tooltip } from '@mui/material'
+import axios from 'axios'
 import { collection, getDocs, query } from 'firebase/firestore'
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
@@ -9,8 +11,20 @@ type Post = {
   imageURL: string
 }
 
+type PostWithAuthor = {
+  author: number
+  caption: string
+  imageURL: string
+  discordId: string
+  error: boolean
+  uuid: string
+  discordTag: string
+  discordName: string
+  username: string
+}
+
 type DiscordLookupResult = {
-  discordId: number
+  discordId: string
   error: boolean
   uuid: string
   discordTag: string
@@ -21,12 +35,35 @@ type DiscordLookupResult = {
 const fstore = firestore.getFirestore()
 
 const Photos: NextPage = () => {
-  const [posts, setPosts] = useState<Post[]>([])
+  const [posts, setPosts] = useState<PostWithAuthor[]>([])
   const [loading, setLoading] = useState(true)
   async function getPosts() {
     try {
       const q = query(collection(fstore, 'photos'))
-      setPosts((await (await getDocs(q)).docs.map((d) => d.data())) as Post[])
+      const posts = (await (
+        await getDocs(q)
+      ).docs.map((d) => d.data())) as Post[]
+
+      const postsWithAuthors: PostWithAuthor[] = await Promise.all(
+        posts.map(async (post) => {
+          // get the author by using axios to request https://tristansmp.com/api/discordLookup?id=:author
+          const author = await axios
+            .get(`https://tristansmp.com/api/discordLookup?id=${post.author}`)
+            .then((res) => res.data)
+            .catch(() => ({
+              discordId: post.author,
+              error: true,
+              uuid: '',
+              discordTag: '',
+              discordName: '',
+              username: ''
+            }))
+
+          return { ...post, ...author }
+        })
+      )
+
+      setPosts(postsWithAuthors)
 
       setLoading(false)
     } catch (error) {
@@ -53,7 +90,7 @@ const Photos: NextPage = () => {
         <section className="px-2 py-32 md:px-0">
           <div className="container items-center px-8 mx-auto xl:px-5">
             <div className="flex flex-wrap items-center sm:-mx-3">
-              {posts.map((post: Post) => (
+              {posts.map((post: PostWithAuthor) => (
                 <div
                   className="w-1/2 px-3 mb-6"
                   key={`${post.author}-${post.caption}`}
@@ -67,9 +104,11 @@ const Photos: NextPage = () => {
                       />
                     </div>
                   </div>
-                  <p className="text-2xl text-center text-secondary">
-                    {post.caption}
-                  </p>
+                  <Tooltip title={`${post.discordTag}`}>
+                    <p className="text-2xl text-center text-secondary">
+                      "{post.caption}" - {post.username}
+                    </p>
+                  </Tooltip>
                 </div>
               ))}
             </div>
