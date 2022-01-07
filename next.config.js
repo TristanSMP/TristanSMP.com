b4e9b2270a8500887aff1632e4b8577f21087289
Tristan Camejo <twisttaan@icloud.com>
2022-01-07 11:57:16 +1100

    feat: redirect tristansmp.com/imagine-griefing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.tsx')
+const withNextra = require("nextra")("nextra-theme-blog", "./theme.config.tsx");
 module.exports = withNextra({
   images: {
-    domains: ['cdn.discordapp.com']
+    domains: ["cdn.discordapp.com"]
+  },
+  async redirects() {
+    return [
+      {
+        source: "/imagine-griefing",
+        destination:
+          "https://cdn.discordapp.com/attachments/871528894623273000/917328471213084722/imagine_griefing.mp4",
+        permanent: true
+      }
+    ];
   }
-})
+});
