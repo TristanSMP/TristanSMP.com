6d98284decabc91bfe028cbd92fe33c242232bb1
Tristan Camejo <twisttaan@icloud.com>
2022-03-28 17:22:19 +1100

    feat: evie tail

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="d42f494a-6a54-4419-8ba0-4541c9c82651"
+          src="https://tail.evie.pw/umami.js"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
