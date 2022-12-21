77629b97820ecdcadcb4e463210301e3d39d4529
Tristan Camejo <contact@tristancamejo.com>
2022-12-21 22:20:20 +1100

    fix: use pipe

--- pages/api/discordLookup.ts
+++ pages/api/discordLookup.ts
@@ -1,21 +1,21 @@
-import axios from 'axios'
-import { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { id } = req.query
+  const { id } = req.query;
   if (!id) {
-    res.status(400).json({ error: 'id is required' })
-    return
+    res.status(400).json({ error: "id is required" });
+    return;
   }
   try {
     const response = await axios.get(
-      `https://api.tristansmp.com/discord/users/id/${id}/player`
-    )
+      `https://pipe.tristansmp.com/discord/users/id/${id}/player`
+    );
 
-    const json = await response.data
-    res.json(json)
+    const json = await response.data;
+    res.json(json);
   } catch {
-    return res.status(404).json({ error: 'id not found' })
+    return res.status(404).json({ error: "id not found" });
   }
-}
+};
